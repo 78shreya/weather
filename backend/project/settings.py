4f3924f24bdf4d6d7b059e47c54df645166efd51
78shreya <shreyadevang25@gmail.com>
2025-03-17 15:07:30 +0530

    added allowed_hosts

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-py_rs^6d(z%5&zcqx(zw%*rq0+j=711f3wczoj7=8ph4gy-hsp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["weather-5i1d.onrender.com"]
 
 
 # Application definition
